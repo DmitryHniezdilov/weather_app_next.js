203d7cbd506173cbe678e3e3e6571c43862ffaed
DmitryHniezdilov <hniezdilovd@gmail.com>
2021-06-05 23:09:03 +0300

    change url to git

--- components/footer/index.js
+++ components/footer/index.js
@@ -24,7 +24,7 @@ const Footer = () => {
                     </Typography>
                     <IconButton
                         component = { Link }
-                        href = 'https://weather-next-hniezdilov.netlify.app/'
+                        href = 'https://github.com/DmitryHniezdilov/weather_app_next.js'
                         rel = 'noreferrer'
                         size = 'small'
                         style = {{color: '#d1d5da'}}
